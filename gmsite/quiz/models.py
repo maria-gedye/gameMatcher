db06d9e0eb89716cb79c280863d86c557e87a3c4
mariagedye <mariagedye@gmail.com>
2022-05-06 11:25:54 +1200

    Adding first question and choices through python shell API

--- gmsite/quiz/models.py
+++ gmsite/quiz/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+
 # Create 2 models here: Question and Choice
 # models are represented by python classes
 
+# USE python manage.py makemigrations to create migrations for any changes below
+# USE python manage.py migrate to apply those changes to the database
+
 class Question(models.Model):
-    question_text = models.CharField(max_length = 250)
+    question_text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -13,3 +20,5 @@ class Choice(models.Model):
     # change below to work for a scale choice (instead of a bi-choice)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
