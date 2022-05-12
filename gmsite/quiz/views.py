35f1395484732ca9156452e30a65fa2e73d8a4e9
mariagedye <mariagedye@gmail.com>
2022-05-13 09:48:09 +1200

    server is running, questions displaying

--- gmsite/quiz/views.py
+++ gmsite/quiz/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
+from django.http import Http404
 
 
 # writing my first view
 def index(request):
-    return HttpResponse("kia ora tatou...Quiz home page here")
+    latest_question_list = Question.objects.order_by('id')[:10]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'quiz/index.html', context)
 
 
 # let's write more views (web pages) this time they take an argument
 def detail(request, question_id):
-    return HttpResponse("Question " % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'quiz/detail.html', {'question': question})
 
 
 def results(request):
