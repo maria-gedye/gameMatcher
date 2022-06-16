1ecc68c90555a281b5922b6301d0713cda979feb
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2022-06-17 10:34:51 +1200

    Delete gameMatcher-olivia directory

--- gameMatcher-olivia/gmsite/gmsite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-
-urlpatterns = [
-    # point the root URLconf at the quiz.urls module
-    path('quiz/', include('quiz.urls')),
-    path('admin/', admin.site.urls),
-]
--- gameMatcher-olivia/gmsite/quiz/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Question
-from .models import Choice
-
-# tell admin that question objects have an admin interface...
-# aka register your models to admin
-admin.site.register(Question)
-admin.site.register(Choice)
-
