e28a4219ffcaf1269fae9d7be0bafdde20b01da5
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2022-06-20 09:12:59 +1200

    pushing

--- gmsite/quiz/urls.py
+++ gmsite/quiz/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import (
     QuizListView,
     quiz_view,
@@ -9,7 +10,7 @@ from .views import (
 app_name = 'quizzes'
 
 urlpatterns = [
-    path('', QuizListView.as_view(), name='main-view'),
+    path('', TemplateView.as_view(template_name="quiz/MainMenu.html"), name='main-view'),
     # primary key is important and is how we retrieve the quiz object
     path('<pk>/', quiz_view, name='quiz-view'),
     path('<pk>/save/', save_quiz_view, name='save-view'),
